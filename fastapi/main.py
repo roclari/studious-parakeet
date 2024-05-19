61bc649072de32df9b5e0d4994c693405c823714
roclari <1201rissa@gmail.com>
2024-05-19 16:36:44 -0300

    base commit

---
+++ fastapi/main.py
@@ -0,0 +1,45 @@
+from typing import Annotated, Union
+
+from fastapi import (
+    Cookie,
+    Depends,
+    FastAPI,
+    Query,
+    WebSocket,
+    WebSocketException,
+    status,
+)
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
+
+
+async def get_cookie_or_token(
+    websocket: WebSocket,
+    session: Annotated[Union[str, None], Cookie()] = None,
+    token: Annotated[Union[str, None], Query()] = None,
+):
+    if session is None and token is None:
+        raise WebSocketException(code=status.WS_1008_POLICY_VIOLATION)
+    return session or token
+
+
+@app.websocket("/items/{item_id}/ws")
+async def websocket_endpoint(
+    *,
+    websocket: WebSocket,
+    item_id: str,
+    q: Union[int, None] = None,
+    cookie_or_token: Annotated[str, Depends(get_cookie_or_token)],
+):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(
+            f"Session cookie or query token value is: {cookie_or_token}"
+        )
+        if q is not None:
+            await websocket.send_text(f"Query parameter q is: {q}")
+        await websocket.send_text(f"Message text was: {data}, for item ID: {item_id}")
